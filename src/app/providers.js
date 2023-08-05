0fe788197b70d206786ccafc8815c7f99d2e8a53
wajid <wajidsiddiqi12@gmail.com>
2023-08-05 22:54:43 +0500

    familykit setup

--- src/app/providers.js
+++ src/app/providers.js
@@ -1,52 +1,25 @@
 "use client";
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultWallets,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import {
-    polygon
-} from 'wagmi/chains';
-import { alchemyProvider } from 'wagmi/providers/alchemy';
-import { publicProvider } from 'wagmi/providers/public';
-import React, { useState, useEffect } from 'react';
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider } from '@chakra-ui/react';
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { createConfig, WagmiConfig } from "wagmi";
+import { polygon } from "wagmi/chains";
+import React, { useState, useEffect } from "react";
 
-const { chains, publicClient } = configureChains(
-    [polygon],
-    [
-        alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_ID }),
-        publicProvider()
-    ]
+const config = createConfig(
+  getDefaultConfig({
+    appName: "portfolio project frontend",
+    alchemyId: process.env.NEXT_PUBLIC_ALCHEMY_ID,
+    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+    chains: [polygon],
+  })
 );
 
-const { connectors } = getDefaultWallets({
-    appName: 'portfolio project frontend',
-    projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
-    chains
-});
-
-const wagmiConfig = createConfig({
-    autoConnect: true,
-    connectors,
-    publicClient
-});
-
 export function Providers({ children }) {
-    const [mounted, setMounted] = useState(false);
-    useEffect(() => setMounted(true), []);
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
 
-    return (
-        <WagmiConfig config={wagmiConfig}>
-            <RainbowKitProvider chains={chains}>
-                <CacheProvider>
-                    <ChakraProvider>
-                        {mounted && children}
-                    </ChakraProvider>
-                </CacheProvider>
-            </RainbowKitProvider>
-        </WagmiConfig>
-    );
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>{mounted && children}</ConnectKitProvider>
+    </WagmiConfig>
+  );
 }

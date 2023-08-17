fd418995c66a8321e78bc939a27711656dd883ff
wajid <wajidsiddiqi12@gmail.com>
2023-08-17 20:16:57 +0500

    added reposiveness to images & to boxe

--- src/app/components/Team.js
+++ src/app/components/Team.js
@@ -5,25 +5,11 @@ const PageWrapper = styled.div`
   background-color: #a474c6;
   position: relative;
   overflow: hidden;
-  padding: 0px 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 0rem 2rem 6.5rem 2rem;
-`;
-
-const TeamSection = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
   flex-direction: column;
-
-  @media (max-width: 895px) {
-    height: auto;
-  }
+  padding: 0rem 2rem 6.5rem 2rem;
 `;
 
 const H1Title = styled.h1`
@@ -36,13 +22,13 @@ const H1Title = styled.h1`
 `;
 
 const TeamContent = styled.div`
-  width: 60%;
-  max-width: 1600px;
+  grid-template-columns: repeat(3, 1fr);
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(150px, auto));
-  align-items: center;
   gap: 2rem;
-  text-align: center;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  }
 `;
 
 const Box = styled.div`
@@ -51,6 +37,10 @@ const Box = styled.div`
   padding: 2rem;
   box-shadow: black 0px 3px;
   border-radius: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   &:hover {
     transform: translateY(-10px);
@@ -59,9 +49,11 @@ const Box = styled.div`
 `;
 
 const Icons = styled.i`
-  display: inline-block;
-  margin: 0 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   transition: all 0.38s ease;
+  margin: 1rem;
 
   &:hover {
     transform: scale(1.2);
@@ -72,7 +64,7 @@ const ImageStyle = {
   width: "100%",
   height: "auto",
   borderRadius: "5rem",
-  marginBottom: "1rem",
+  marginBottom: "2rem",
 };
 
 const H3Title = styled.h2`
@@ -86,7 +78,7 @@ const H3Title = styled.h2`
 
 const Para = styled.p`
   color: #a474c6;
-  font-size: 1rem;
+  font-size: 1.4rem;
   margin: 1rem;
   line-height: 1;
   text-shadow: black 0px 1px;
@@ -96,81 +88,83 @@ export default function Team() {
   return (
     <PageWrapper>
       <H1Title>Our Team</H1Title>
-      <TeamSection>
-        <TeamContent>
-          <Box>
-            <Image
-              src="/assets/nfts/1.png"
-              alt="Team Member"
-              style={ImageStyle}
-              width={0}
-              height={0}
-            />
-            <H3Title>Peter Joe</H3Title>
-            <Para>Founder</Para>
-            <Icons>
-              <a href="#">
-                <i>
-                  <Image
-                    src="/assets/icons/twitter-x.svg"
-                    width="25"
-                    height="25"
-                    alt="Twitter"
-                  />
-                </i>
-              </a>
-            </Icons>
-          </Box>
 
-          <Box>
-            <Image
-              src="/assets/nfts/2.png"
-              alt="Team Member"
-              style={ImageStyle}
-              width={0}
-              height={0}
-            />
-            <H3Title>Wajid</H3Title>
-            <Para>Developer</Para>
-            <div>
-              <a href="https://twitter.com/abdulwajidsid" target="_blank">
-                <Icons>
-                  <Image
-                    src="/assets/icons/twitter-x.svg"
-                    width="25"
-                    height="25"
-                    alt="Twitter"
-                  />
-                </Icons>
-              </a>
-            </div>
-          </Box>
+      <TeamContent>
+        <Box>
+          <Image
+            src="/assets/nfts/7.png"
+            alt="Team Member"
+            style={ImageStyle}
+            width={0}
+            height={0}
+            layout="responsive"
+          />
+          <H3Title>Peter Joe</H3Title>
+          <Para>Founder</Para>
+          <Icons>
+            <a href="#">
+              <i>
+                <Image
+                  src="/assets/icons/twitter-x.svg"
+                  width="35"
+                  height="35"
+                  alt="Twitter"
+                />
+              </i>
+            </a>
+          </Icons>
+        </Box>
 
-          <Box>
-            <Image
-              src="/assets/nfts/3.png"
-              alt="Team Member"
-              style={ImageStyle}
-              width={0}
-              height={0}
-            />
-            <H3Title>Tom Luka</H3Title>
-            <Para>Artist</Para>
-            <Icons>
-              <a href="#">
-                <i>
-                  <Image
-                    src="/assets/icons/twitter-x.svg"
-                    width="25"
-                    height="25"
-                    alt="Twitter"
-                  />
-                </i>
-              </a>
-            </Icons>
-          </Box>
-        </TeamContent>
-      </TeamSection>
+        <Box>
+          <Image
+            src="/assets/nfts/16.png"
+            alt="Team Member"
+            style={ImageStyle}
+            width={0}
+            height={0}
+            unoptimized
+          />
+          <H3Title>Wajid</H3Title>
+          <Para>Developer</Para>
+          <div>
+            <a href="https://twitter.com/abdulwajidsid" target="_blank">
+              <Icons>
+                <Image
+                  src="/assets/icons/twitter-x.svg"
+                  width="35"
+                  height="35"
+                  alt="Twitter"
+                />
+              </Icons>
+            </a>
+          </div>
+        </Box>
+
+        <Box>
+          <Image
+            src="/assets/nfts/2.png"
+            alt="Team Member"
+            style={ImageStyle}
+            width={0}
+            height={0}
+            unoptimized
+          />
+          <H3Title>Tom Luka</H3Title>
+          <Para>Artist</Para>
+          <Icons>
+            <a href="#">
+              <i>
+                <Image
+                  src="/assets/icons/twitter-x.svg"
+                  width="35"
+                  height="35"
+                  alt="Twitter"
+                />
+              </i>
+            </a>
+          </Icons>
+        </Box>
+      </TeamContent>
     </PageWrapper>
   );
 }

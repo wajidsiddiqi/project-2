840a16a1f24c3c3da244fe34fa36716ad26683cd
wajid <wajidsiddiqi12@gmail.com>
2023-08-21 22:53:18 +0500

    added headings, para & button

--- src/app/styles/styles.js
+++ src/app/styles/styles.js
@@ -23,4 +23,74 @@ const PageWrapperLight = styled(PageWrapperDark)`
   background-color: #eef3f1;
 `;
 
-export { MainPageWrapper, PageWrapperDark, PageWrapperLight };
+const MainH1Title = styled.h1`
+  color: #a474c6;
+  font-size: 3.5rem;
+  text-transform: uppercase;
+  font-family: "04b30";
+  margin: 1rem;
+  text-shadow: black 0px 3px;
+`;
+
+const H1TitleLight = styled(MainH1Title)`
+  color: #eef3f1;
+  margin: 0;
+  padding: 6.5rem;
+`;
+
+const H1TitleDark = styled(H1TitleLight)`
+  color: #a474c6;
+`;
+
+const H2TitleDark = styled.h2`
+  color: #a474c6;
+  font-size: 2.5rem;
+  text-transform: uppercase;
+  font-family: "04b30";
+  margin: 1rem;
+  text-shadow: black 0px 3px;
+`;
+
+const ParaBig = styled.p`
+  color: #a474c6;
+  font-size: 2rem;
+  margin: 1rem;
+  line-height: 1;
+  text-shadow: black 0px 1px;
+`;
+
+const ParaMid = styled.p`
+  font-size: 1.5rem;
+`;
+
+const StyledButton = styled.button`
+  padding: 0.7rem 1.4rem;
+  color: #eef3f1;
+  cursor: pointer;
+  border-radius: 5rem;
+  font-family: inherit;
+  background-color: #a474c6;
+  border: none;
+  font-size: 1.2rem;
+  transition: background-color 0.3s, color 0.3s;
+  box-shadow: black 0px 3px;
+  margin: 1rem;
+
+  &:hover {
+    background-color: #eef3f1;
+    color: #a474c6;
+  }
+`;
+
+export {
+  MainPageWrapper,
+  PageWrapperDark,
+  PageWrapperLight,
+  MainH1Title,
+  H1TitleLight,
+  H1TitleDark,
+  H2TitleDark,
+  ParaBig,
+  ParaMid,
+  StyledButton,
+};

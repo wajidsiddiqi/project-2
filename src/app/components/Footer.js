428f069fba6990ccdeed449d09b55b992f709aa3
wajid <wajidsiddiqi12@gmail.com>
2023-08-24 01:39:12 +0500

    organized footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,31 +1,12 @@
 "use client";
-import styled from "styled-components";
 import Image from "next/image";
-
-const Center = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const StyledFooter = styled.footer`
-  background-color: #eef3f1;
-  padding: 0.5rem 2.5rem;
-`;
-
-const Para = styled.p`
-  color: #a474c6;
-  font-size: 1.4rem;
-  margin: 1rem 0rem 1rem 1rem;
-  line-height: 1;
-  text-shadow: black 0px 1px;
-`;
+import { Center, FooterPara, StyledFooter } from "@/app/styles/styles.js";
 
 export default function Footer() {
   return (
     <StyledFooter>
       <Center>
-        <Para>Developed with love by Wajid © 2023 |</Para>
+        <FooterPara>Developed with love by Wajid © 2023 |</FooterPara>
         <a href="https://twitter.com/abdulwajidsid" target="_blank">
           <Image
             src="/assets/icons/twitter-x.svg"

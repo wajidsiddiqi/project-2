9569df1d050e4e5087cf3fb2823fc1bac0ed93c1
wajid <wajidsiddiqi12@gmail.com>
2023-08-24 01:31:55 +0500

    organized team

--- src/app/components/Team.js
+++ src/app/components/Team.js
@@ -1,96 +1,22 @@
-import styled from "styled-components";
 import Image from "next/image";
 
-const MaxWidth = styled.div`
-  max-width: 1600px;
-`;
-
-const PageWrapper = styled.div`
-  background-color: #a474c6;
-  position: relative;
-  overflow: hidden;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  padding: 0rem 2rem 6.5rem 2rem;
-`;
-
-const H1Title = styled.h1`
-  color: #eef3f1;
-  font-size: 3.5rem;
-  text-transform: uppercase;
-  font-family: "04b30";
-  padding: 6.5rem;
-  text-shadow: black 0px 3px;
-`;
-
-const TeamContent = styled.div`
-  grid-template-columns: repeat(3, 1fr);
-  display: grid;
-  gap: 2rem;
-
-  @media (max-width: 768px) {
-    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  }
-`;
-
-const Box = styled.div`
-  transition: all 0.38s ease;
-  background-color: #eef3f1;
-  padding: 2rem;
-  box-shadow: black 0px 3px;
-  border-radius: 5rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  &:hover {
-    transform: translateY(-10px);
-    cursor: pointer;
-  }
-`;
-
-const Icons = styled.i`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  transition: all 0.38s ease;
-
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-
-const ImageStyle = {
-  width: "100%",
-  height: "auto",
-  borderRadius: "5rem",
-  marginBottom: "2rem",
-};
-
-const H3Title = styled.h2`
-  color: #a474c6;
-  font-size: 1.5rem;
-  text-transform: uppercase;
-  font-family: "04b30";
-  text-shadow: black 0px 3px;
-`;
-
-const Para = styled.p`
-  color: #a474c6;
-  font-size: 1.4rem;
-  margin: 1rem;
-  line-height: 1;
-  text-shadow: black 0px 1px;
-`;
+import {
+  PageWrapperDark,
+  H1TitleLight,
+  MaxWidth,
+  H3Title,
+  ParaSm,
+  TeamContent,
+  Icons,
+  ImageStyle,
+  Box,
+} from "@/app/styles/styles.js";
 
 export default function Team() {
   return (
-    <PageWrapper>
+    <PageWrapperDark>
       <MaxWidth>
-        <H1Title>Our Team</H1Title>
+        <H1TitleLight>Our Team</H1TitleLight>
 
         <TeamContent>
           <Box>
@@ -169,6 +95,6 @@ export default function Team() {
           </Box>
         </TeamContent>
       </MaxWidth>
-    </PageWrapper>
+    </PageWrapperDark>
   );
 }
